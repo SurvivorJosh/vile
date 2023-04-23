22cc821dc4d56646622be56051189fdccf5a85f3
Lone <94772757+SurvivorJosh@users.noreply.github.com>
2023-04-23 13:56:26 -0400

    Update vile.py

--- vile.py
+++ vile.py
@@ -328,6 +328,6 @@ async def main():
         await main()
 
 if __name__ == "__main__":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    
     asyncio.run(main())
     
